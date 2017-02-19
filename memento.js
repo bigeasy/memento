22b5adbc2c69214e67f84868048420b694938732
Alan Gutierrez <alan@prettyrobots.com>
2017-02-19 10:26:45 -0600

    Truely working bootstrap and welcome.
    Went over it all again after simplifying compassion's concepts of out of
    band data and application logging.

--- memento.js
+++ memento.js
@@ -7,41 +7,43 @@ function Memento (cliffhanger, nodes) {
     this._nodes = nodes
     this._index = 0
     this._cliffhanger = cliffhanger
+    this._stores = {}
 }
-
-Memento.prototype.bootstrap = cadence(function (async, conference) {
-    console.log('bootstrapping')
-    conference.ifNotReplaying(async)(function () {
-        async(function () {
-            crypto.randomBytes(16, async())
-        }, function (buffer) {
-            conference.record('catalog', buffer.toString('hex'), async())
-        })
-    })
-})
 
 Memento.prototype.join = cadence(function (async, conference) {
     async(function () {
-        this.bootstrap(conference, async())
-    }, function () {
-        conference.request('store', async())
+        conference.record_(async)(function () {
+            conference.request('store', { promise: conference.government.promise }, async())
+        })
     }, function (store) {
         this._nodes = store.nodes
         this._index = store.index
-    }, function (store) {
-        conference.boundary()
     })
 })
 
-Memento.prototype.immigrate = cadence(function (async, conference) {
-    conference.makeWelcome({
+// Actually, let's just have a naturalize event. Ah, no, because we may
+// naturalize or else we may exile before we naturalize.
+Memento.prototype.immigrate = cadence(function (async, conference, id) {
+    this._stores[conference.government.promise] = {
         nodes: JSON.parse(JSON.stringify(this._nodes)),
         index: this._index
-    })
+    }
 })
 
-Memento.prototype.store = cadence(function (async, promise, id) {
-    return conference.getWelcome(promise)
+Memento.prototype._clearStore = function (promise) {
+    delete this._stores[promise]
+}
+
+Memento.prototype.naturalize = cadence(function (async, conference, promise) {
+    this._clearStore(promise)
+})
+
+Memento.prototype.exile = cadence(function (async, conference, id) {
+    this._clearStore(conference.government.exile.promise)
+})
+
+Memento.prototype.store = cadence(function (async, conference, body) {
+    return [ this._stores[body.promise] ]
 })
 
 Memento.prototype.set = cadence(function (async, conference, envelope) {
@@ -57,7 +59,7 @@ Memento.prototype.set = cadence(function (async, conference, envelope) {
     }
 })
 
-Memento.prototype.delete = cadence(function (async, set) {
+Memento.prototype.remove = cadence(function (async, set) {
     var node = this.nodes[set.path]
     if (envelope.from == this.paxos.id) {
         var result = {
--- run/quiet.sh
+++ run/quiet.sh
@@ -1,3 +1,3 @@
-    node node_modules/compassion.colleague/colleague.bin.js --chaperon http://127.0.0.1:8286 --conduit http://127.0.0.1:8486 --island island --id 1 \
+    node node_modules/compassion.colleague/colleague.bin.js --chaperon http://127.0.0.1:8286 --conduit http://127.0.0.1:8486 --island island --id $1 \
         node_modules/.bin/prolific --inherit COMPASSION_COLLEAGUE_FD stdio \
-            node memento.bin.js --bind 8080
+            node memento.bin.js --bind 808$1
